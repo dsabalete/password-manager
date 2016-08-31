3918b04c49dd49cc1050339bb1e6c667d59a6fac
David Sabalete <dsabalete@gmail.com>
2016-08-31 15:46:12 +0200

    Initial commit

---
+++ example-json.js
@@ -0,0 +1,30 @@
+var person = {
+    name: 'David',
+    age: 43
+};
+
+var personJSON = JSON.stringify(person);
+
+console.log(personJSON);
+console.log(typeof personJSON);
+
+var personObject = JSON.parse(personJSON);
+
+console.log(personObject.name);
+console.log(typeof personObject);
+
+console.log('CHALLENGE AREA');
+
+var animal = '{"name": "Halley"}';
+
+// convert to js object
+var animalObject = JSON.parse(animal);
+
+// add age prop
+animalObject.age = 3;
+
+// convert back to JSON
+animal = JSON.stringify(animalObject);
+
+// log out
+console.log(animal);
